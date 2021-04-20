6a5aaf2b9b9e49632261c1c0a179200ff8698c3a
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-20 19:28:26 +0530

    multiple insertion

--- Inserting Data/Insert.sql
+++ Inserting Data/Insert.sql
@@ -2,16 +2,21 @@
 
 CREATE TABLE cats(name VARCHAR(50), age INT);
 
-INSERT INTO cats(name, 
-                age)
+INSERT INTO cats(name, age)
 
-            VALUES('Blue', 
-                    1);
+            VALUES('Blue', 1);
 
-INSERT INTO cats(age, 
-                name)
+INSERT INTO cats(age,  name)
 
-            VALUES(11, 
-                'DRACO');
-/* 
-SELECT * FROM cats; */
+            VALUES(11, 'DRACO');
+
+SELECT * FROM cats;
+
+
+INSERT INTO cats(name, age)
+
+            VALUES('Peanut', 7),
+            ('Butter', 4),
+            ('Jelly', 2);
+
+SELECT * FROM cats;
