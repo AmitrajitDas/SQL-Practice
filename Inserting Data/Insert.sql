46cacf0e112d31b94acd99b9ada575367e10df9c
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-20 21:11:56 +0530

    primary keys and auto increment

--- Inserting Data/Insert.sql
+++ Inserting Data/Insert.sql
@@ -79,3 +79,33 @@ SELECT * FROM cats2;
  INSERT INTO cats3() VALUES();
 
  SELECT * FROM cats3;
+
+ /* Primary Key */
+
+ CREATE TABLE unique_cats(cat_id INT NOT NULL, name VARCHAR(100), age INT, PRIMARY KEY (cat_id));
+
+ INSERT INTO unique_cats(cat_id, name, age) VALUES(2, "Louise", 23);
+
+ INSERT INTO unique_cats(cat_id, name, age) VALUES(1, "Fred", 2);
+
+ SELECT * FROM unique_cats;
++--------+--------+------+
+| cat_id | name   | age  |
++--------+--------+------+
+|      1 | Fred   |    2 |
+|      2 | Louise |   23 |
++--------+--------+------+
+
+CREATE TABLE unique_cats2(cat_id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100), age INT, PRIMARY KEY (cat_id)); /* using autoincrement */
+
+INSERT INTO unique_cats2(name, age) VALUES("Jim", 3);
+
+INSERT INTO unique_cats2(name, age) VALUES("Skippy", 5);
+
+ SELECT * FROM unique_cats2;
++--------+--------+------+
+| cat_id | name   | age  |
++--------+--------+------+
+|      1 | Jim    |    3 |
+|      2 | Skippy |    5 |
++--------+--------+------+
