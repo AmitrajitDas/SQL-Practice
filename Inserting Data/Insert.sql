4a21b4bc572d89e48ee035ce75762e1655784885
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-20 20:50:01 +0530

    default values

--- Inserting Data/Insert.sql
+++ Inserting Data/Insert.sql
@@ -46,3 +46,36 @@ SHOW WARNINGS;
 INSERT INTO cats() VALUES();
 
 CREATE TABLE cats2(name VARCHAR(50) NOT NULL, age INT NOT NULL); /* NOT NULL */
+
+
+/* DEFAULT */
+CREATE TABLE cats2( name VARCHAR(20) DEFAULT 'no name provided', age INT DEFAULT 99 );
+
+DESC cats2;
+
+INSERT INTO cats2(age) VALUES(13);
+
+SELECT * FROM cats2;
+
++------------------+------+
+| name             | age  |
++------------------+------+
+| no name provided |   13 |
++------------------+------+
+
+INSERT INTO cats2() VALUES();
+
+SELECT * FROM cats2;
+
++------------------+------+
+| name             | age  |
++------------------+------+
+| no name provided |   13 |
+| no name provided |   99 |
++------------------+------+
+
+ CREATE TABLE cats3( name VARCHAR(20) NOT NULL DEFAULT 'no name provided', age INT NOT NULL DEFAULT 99 );
+
+ INSERT INTO cats3() VALUES();
+
+ SELECT * FROM cats3;
