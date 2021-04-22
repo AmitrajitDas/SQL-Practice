44e1de4f89753b3ebfe2556ad367060c1a6a9851
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-22 19:50:35 +0530

    challenge solution

---
+++ CRUD/Challenge.sql
@@ -0,0 +1,9 @@
+SELECT cat_id FROM cats; 
+
+SELECT name, breed FROM cats; 
+
+SELECT name, age FROM cats WHERE breed='Tabby'; 
+
+SELECT cat_id, age FROM cats WHERE cat_id=age; 
+
+SELECT * FROM cats WHERE cat_id=age; 
