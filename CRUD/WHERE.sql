a37443c8a2acc3990294826e1f493c55b4277b87
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-22 15:16:19 +0530

    using where command

---
+++ CRUD/WHERE.sql
@@ -0,0 +1,11 @@
+-- Select by age
+
+SELECT * FROM cats WHERE age=4; 
+
+-- Select by name
+
+SELECT * FROM cats WHERE name='Egg'; 
+
+-- Notice how it deals with case
+
+SELECT * FROM cats WHERE name='egG'; 
